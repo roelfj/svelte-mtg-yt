7ec1a63ae4e4038c6043eb545041a838501e5119
roelfj <roelfj@users.noreply.github.com>
2023-05-08 13:12:18 +0200

    Got Components Working

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+    import '../global.css'
+    import player from "./Player.svelte";
+    import Player from "./Player.svelte";
+</script>
+
+<style>
+    main {
+        width: 80%;
+        padding: 20px;
+        border: solid gray 1px;
+        margin: 0 auto;
+        background-color: wheat;
+        margin: 10vh auto;
+    }
+
+    #controls-container {
+        display: flex;
+    }
+
+    button {
+        display: block;
+        width: 100%;
+        margin-top: 20px;
+        border: solid salmon 1px;
+        background-color: sandybrown;
+        color: rgb(61,56,56);
+        font-size: 20px;
+        border-right: 3px;
+    }
+</style>
+
+<main>
+    <h1>The Gathering</h1>
+    <div id="controls-container">
+        <Player/>
+        <Player/>
+    </div>
+    <button>Start Game</button>
+</main>
