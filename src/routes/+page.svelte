54035594beac124ffc38cda2e406b8719b3ca0d5
roelfj <roelfj@users.noreply.github.com>
2023-05-10 14:55:29 +0200

    Playing with fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,24 @@
 <script>
     import './style1.css';
+    import { Jumper } from 'svelte-loading-spinners';
+    import { navigating,page, } from '$app/stores'
+    import { onMount } from 'svelte';
+
+    let now = new Date(), month, day, year;
+    let dateString;
+
+    onMount(()=> {
+        month = '' + (now.getMonth() + 1),
+            day = '' + now.getDate(),
+            year = now.getFullYear();
+
+        if (month.length < 2)
+            month = '0' + month;
+        if (day.length < 2)
+            day = '0' + day;
+
+        dateString = [year, month, day].join('-');
+    })
 
     let questions = [
         { id: 1, text: `Muizenberg` },
@@ -21,7 +40,13 @@
 
 <div id="container">
 
-    <h4>Dirk Surf Report</h4>
+
+    <!--{#if !$page.data}-->
+    <!--    <Jumper size="60" color="#FF3E00" unit="px" duration="1s" />-->
+    <!--{/if}-->
+
+    <h3>Dirk Surf Report</h3>
+    <h6>{dateString}</h6>
 
     <select bind:value={selected} on:change={changetown}>
         {#each questions as question}
@@ -33,8 +58,7 @@
 
     <link href="//www.surf-forecast.com/stylesheets/widget.css" media="screen" rel="stylesheet" type="text/css" />
     <div class="surf-fc-widget" >
-<!--        <div class="widget-container">-->
-<!--            <div class="external-cont">-->
+
                 <iframe
                         title="surf"
                         class="surf-fc-i"
@@ -42,8 +66,6 @@
                         src={surfurl}
                         >
                 </iframe>
-<!--            </div>-->
-<!--        </div>-->
     </div>
 
 
@@ -60,7 +82,7 @@
         transition: all 1s;
         font-family: Helvetica,Arial,sans-serif;
         overflow: auto;
-        background-color: red;
+        /*background-color: red;*/
         border: solid 1px blue;
     }
     .surf-fc-widget .surf-fc-i {
@@ -72,75 +94,3 @@
 </style>
 
 
-<!--<script>-->
-<!--    import '../global.css'-->
-<!--    import Player from "./Player.svelte";-->
-<!--    let redScore = 20;-->
-<!--    let blueScore = 20;-->
-<!--    $ : blueWon = redScore <= 0;-->
-<!--    $ : redWon = blueScore <= 0;-->
-<!--    $:  gameOver = blueWon || redWon;-->
-
-<!--    function updateBlueScore(e) {-->
-<!--         const updateScore = e.detail;-->
-<!--         blueScore += updateScore;-->
-<!--    }-->
-
-<!--    function updateRedScore(e) {-->
-<!--        const updateScore = e.detail;-->
-<!--        redScore += updateScore;-->
-<!--    }-->
-
-
-<!--    function newGame() {-->
-<!--        redScore = 20;-->
-<!--        blueScore = 20;-->
-<!--    }-->
-<!--</script>-->
-
-<!--<style>-->
-<!--    main {-->
-<!--        width: 80%;-->
-<!--        padding: 20px;-->
-<!--        border: solid gray 1px;-->
-<!--        margin: 0 auto;-->
-<!--        background-color: wheat;-->
-<!--        margin: 10vh auto;-->
-<!--    }-->
-
-<!--    #controls-container {-->
-<!--        display: flex;-->
-<!--    }-->
-
-<!--    button {-->
-<!--        display: block;-->
-<!--        width: 100%;-->
-<!--        margin-top: 20px;-->
-<!--        border: solid salmon 1px;-->
-<!--        background-color: sandybrown;-->
-<!--        color: rgb(61,56,56);-->
-<!--        font-size: 20px;-->
-<!--        border-right: 3px;-->
-<!--    }-->
-<!--</style>-->
-
-<!--<main>-->
-<!--    <h1>Roelf Demo</h1>-->
-<!--    <div id="controls-container">-->
-<!--        <Player-->
-<!--                {gameOver}-->
-<!--                on:points={updateBlueScore}-->
-<!--                fontColour=blue-->
-<!--                won={blueWon}-->
-<!--                winningText="Blue Wins"-->
-<!--                score={blueScore}/>-->
-<!--        <Player-->
-<!--                {gameOver}-->
-<!--                on:points={updateRedScore}-->
-<!--                fontColour=red-->
-<!--                won={redWon}-->
-<!--                winningText="Red Wins"-->
-<!--                score={redScore}/>-->
-<!--    </div>-->
-<!--    <button on:click={ newGame } >Start Game</button>-->
-<!--</main>-->
